6e634242fffcca33df79320ec3e8343808d71d2e
MR-1301 <mahir2cool@gmail.com>
2021-04-13 15:31:44 +0530

    solved few dp ques

--- DynamicProgramming/PaintingFence.cpp
+++ DynamicProgramming/PaintingFence.cpp
@@ -12,16 +12,15 @@ class Solution{
     long long countWays(int n, int k){
         // code here
         long long mod=1e9+7;
-        vector<vector<long long> > dp(n,vector<long long>(2,0));
-        dp[0][0]=k;
-        dp[0][1]=0;
-        for(int i=1;i<n;i++)
+        vector<long long>  dp(n,0);
+        dp[0]=k;
+        dp[1]=k*k;
+        for(int i=2;i<n;i++)
         {
-            dp[i][0]=((dp[i-1][0])*k)%mod;
-            dp[i][1]=((dp[i-1][1]*k)%mod+dp[i-1][0])%mod;
+            dp[i]=(((dp[i-1]+dp[i-2])%mod)*(k-1))%mod;    
         }
 
-        return (dp[n][0]+dp[n][1])%mod;
+        return dp[n-1];
     }
 };
 
