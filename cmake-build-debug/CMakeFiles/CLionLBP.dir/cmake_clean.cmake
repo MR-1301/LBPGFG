7609e50bdef6f3f3202b7ac46589acdd59f4abce
MR-1301 <mahir2cool@gmail.com>
2021-05-14 11:39:23 +0530

    "clion config"

---
+++ cmake-build-debug/CMakeFiles/CLionLBP.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CLionLBP.exe"
+  "CLionLBP.exe.manifest"
+  "CLionLBP.pdb"
+  "CMakeFiles/CLionLBP.dir/String/ValidIp.cpp.obj"
+  "libCLionLBP.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/CLionLBP.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
